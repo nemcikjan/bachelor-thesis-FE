a2789a8590cf567c308a5b71a65f667c0698cef6
Jany Nemcik <janynemcik5@gmail.com>
2019-04-11 14:56:23 +0200

    edited deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ echo "transfer started..."
 rsync -r --delete-after $TRAVIS_BUILD_DIR/dist deploy@104.248.18.71:dist
 echo "content transfered"
 
-ssh -t deploy@104.248.18.71 'cd dist/dist; sudo cp --parents ./bachelor-iot-web-app /var/www/html; rm -rf dist; exit'
+ssh -t deploy@104.248.18.71 -y 'cd dist/dist; sudo cp --parents ./bachelor-iot-web-app /var/www/html; rm -rf dist; exit'
