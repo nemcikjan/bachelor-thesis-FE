f2618a724996f204547ec855a863702e9f4fd4f3
Jany Nemcik <janynemcik5@gmail.com>
2019-04-18 23:26:03 +0200

    added toastr module

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -4,10 +4,15 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule, LoadChildrenCallback } from '@angular/router';
 import { MainModuleRouting as MAIN_ROUTES } from './routes/main.routing';
 import { provideServices } from './service';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, RouterModule.forChild(MAIN_ROUTES)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(MAIN_ROUTES),
+    ToastrModule.forRoot()
+  ],
   providers: [...provideServices()]
 })
 export class MainModule {}
