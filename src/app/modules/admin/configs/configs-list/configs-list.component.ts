c6a3396186919d48be9f7fbef21131c245a11da2
Jany Nemcik <janynemcik5@gmail.com>
2019-04-19 15:40:38 +0200

    adding new node

--- src/app/modules/admin/configs/configs-list/configs-list.component.ts
+++ src/app/modules/admin/configs/configs-list/configs-list.component.ts
@@ -11,22 +11,42 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./configs-list.component.scss']
 })
 export class ConfigsListComponent implements OnInit {
-  form: FormGroup;
+  configForm: FormGroup;
+  nodeForm: FormGroup;
   type: string;
   add = false;
+  node = false;
   constructor(
     private route: ActivatedRoute,
     private configsService: ConfigsService
   ) {}
   configs: any[];
   nodes: any[];
+  textMask = {
+    mask: [
+      ...[...Array(5).fill(null)].reduce(
+        prev => [...prev, ...[/[a-zA-Z0-9]/, /[a-zA-Z0-9]/, ':']],
+        []
+      ),
+      ...[/[a-zA-Z0-9]/, /[a-zA-Z0-9]/]
+    ],
+    guide: true,
+    placeholderChar: 'x'
+  };
   ngOnInit() {
     this.type = this.route.snapshot.queryParams['type'];
-    this.form = new FormGroup({
+    this.configForm = new FormGroup({
       nodeType: new FormControl({ value: this.type, disabled: true }, [
         Validators.required
       ]),
       interval: new FormControl({ hour: 0, minute: 1 }, [Validators.required]),
+      nodeId: new FormControl(null, [Validators.required])
+    });
+
+    this.nodeForm = new FormGroup({
+      nodeType: new FormControl({ value: this.type, disabled: true }, [
+        Validators.required
+      ]),
       nodeId: new FormControl(null, [Validators.required])
     });
     this.configsService
@@ -38,17 +58,38 @@ export class ConfigsListComponent implements OnInit {
   }
 
   addConfig() {
-    const { interval, ...rest } = this.form.value;
+    const { interval, nodeId } = this.configForm.value;
     const c = val => (val < 10 ? '0' + val : '' + val);
     const value = {
-      ...rest,
+      nodeId,
       nodeType: this.type,
       interval: `${c(interval.hour)}-${c(interval.minute)}`
     };
-    this.configsService.insertConfig(value).subscribe();
+    this.configsService
+      .insertConfig(value)
+      .pipe(mergeMap(() => this.configsService.getConfigsByType(this.type)))
+      .subscribe({
+        next: configs => {
+          this.configs = configs;
+          this.add = false;
+          this.configForm.reset();
+        }
+      });
   }
 
-  addNode() {}
+  addNode() {
+    const node = { ...this.nodeForm.value, nodeType: this.type };
+    this.configsService
+      .insertNode(node)
+      .pipe(mergeMap(() => this.configsService.getAllNodes(this.type)))
+      .subscribe({
+        next: nodes => {
+          this.nodes = nodes;
+          this.node = false;
+          this.nodeForm.reset();
+        }
+      });
+  }
 
   submitMethod() {}
 }
